2da3382f740a6dca6aa6e4dda0386a8555d509d0
RasitAkay11 <Rasittt55@gmail.com>
2025-06-09 16:25:48 +0200

    setup game loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 
 int main() 
 {
+    Color grey = {29,29,27,255};
     InitWindow(750, 700, "C++ space shooters! Made by Rasit Akay..");
-    
+    SetTargetFPS(144);
+
+    //This function returns true if the ESC button on the keyboard or in the game is pressed.
+    while(WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(grey);
+        EndDrawing();
+    }
+
     CloseWindow();
 }
