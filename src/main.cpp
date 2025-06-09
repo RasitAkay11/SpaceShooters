c057e2a3fe86115530db0efc6d963c6a46fab5fc
RasitAkay11 <Rasittt55@gmail.com>
2025-06-09 16:05:57 +0200

    remove template to make an simpler base

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,19 @@
 #include <raylib.h>
-#include "ball.h"
 
 int main() 
 {
-    const Color darkGreen = {20, 160, 133, 255};
-    
+    //Raylib window settings
+    const Color BackgroundColor = {0, 0, 0, 255};
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 600;
     
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "C++ space shooters! Made by Rasit Akay..");
     SetTargetFPS(60);
     
     while (!WindowShouldClose())
-    {
-        ball.Update();
-        
+    {   
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+            ClearBackground(BackgroundColor);
         EndDrawing();
     }
     
